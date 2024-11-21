a714a45f17168c6dff6ffdf5f90203dc432ee7bf
majood987 <majdali987987@gmail.com>
2024-11-20 18:53:36 -0800

    Delete Kadnya/Calendars directory

--- Kadnya/Calendars/Calendly/config.py
+++
@@ -1,7 +0,0 @@
-from Kadenya import settings
-
-clientId = settings.CALENDLY_CLIENT_ID
-clientSecret = settings.CALENDLY_CLIENT_SECRET
-sp = 'calendly'
-
-userEndPointUuid = 'https://api.calendly.com/users/'
