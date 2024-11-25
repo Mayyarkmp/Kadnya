21101c0883a9c3f99bea0473663478ea5e578775
majood987 <majdali987987@gmail.com>
2024-11-25 07:06:47 -0800

    Delete Kadnya/Calendars directory

--- Kadnya/Calendars/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-from Kadnya import settings
-
-
-class CalendarsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "Calendars"
-    oAuthRedirectUrl = settings.OAUTH_REDIRECT_URI
