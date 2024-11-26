7713d0f0db591f81a8037e6bbafd39b04d1294de
Mayyar Saied <73830238+Mayyarkmp@users.noreply.github.com>
2024-11-26 23:29:28 +0300

    Delete Kadnya/Kadnya/Kadnya directory
    No idea where this directory came from

--- Kadnya/Kadnya/Kadnya/api.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from ninja import NinjaAPI, Schema
-from PaymentGateways.api import tapApi
-
-# app_name = "Kadnya"
-# urlpatterns = [path("Integration/", tapApi.urls)]
-
-# api = NinjaAPI()
