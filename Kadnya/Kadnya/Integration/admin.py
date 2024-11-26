8868e8db355ea854a7f7efb4e82f57568f9c535d
Mayyar Saied <73830238+Mayyarkmp@users.noreply.github.com>
2024-11-26 23:33:50 +0300

    Delete Kadnya/Kadnya/Integration directory
    Don't know who added this

--- Kadnya/Kadnya/Integration/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Lead, License, Charge, File
-# Register your models here.
-
-admin.site.register(Lead)
-admin.site.register(License)
-admin.site.register(Charge)
-admin.site.register(File)
